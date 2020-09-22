6446617792775546ab8b5626247ebf45f6f8d7cb
Jeffrey Ryan Walker <jryanwalker93@gmail.com>
2020-09-22 11:37:23 -0500

    Update host in ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,20 @@
 module.exports = {
-  apps: [{
-    name: 'review_service_rw',
-    script: './server/index.js',
-  }],
+  apps: [
+    {
+      name: "review_service_rw",
+      script: "./server/index.js",
+    },
+  ],
   deploy: {
     production: {
-      user: 'ubuntu',
-      host: 'ec2-18-218-79-61.us-east-2.compute.amazonaws.com',
-      key: '~/.ssh/FEC-BestBuy-RW.pem',
-      ref: 'origin/master',
-      repo: 'https://github.com/ATX-50-Team-Best-Buy/review_service_rw.git',
-      path: '/home/ubuntu/review-service',
-      'post-deploy': 'npm install && npm run build && pm2 startOrRestart ecosystem.config.js',
+      user: "ubuntu",
+      host: "ec2-3-14-15-3.us-east-2.compute.amazonaws.com/",
+      key: "~/.ssh/FEC-BestBuy-RW.pem",
+      ref: "origin/master",
+      repo: "https://github.com/ATX-50-Team-Best-Buy/review_service_rw.git",
+      path: "/home/ubuntu/review-service",
+      "post-deploy":
+        "npm install && npm run build && pm2 startOrRestart ecosystem.config.js",
     },
   },
 };
